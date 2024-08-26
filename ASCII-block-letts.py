6a062c101e14997557191a6d6635fb2d6e2c842d
linkwithlionhart <linkwithlionhart@gmail.com>
2024-08-26 12:50:26 -0700

    afternoon practice

---
+++ ASCII-block-letts.py
@@ -0,0 +1,11 @@
+# WL
+# I like to code.
+
+print('1 2 3 4 5   1 2 3 4 5')
+print('W       W   L')
+print('W       W   L')
+print('W       W   L')
+print('W   W   W   L')
+print('W W   W W   L')
+print('W W   W W   L')
+print('W       W   L L L L L')
