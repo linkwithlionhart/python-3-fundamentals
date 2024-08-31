561fb03ed531a708d0dc9e914f98c6e3aaede95b
linkwithlionhart <linkwithlionhart@gmail.com>
2024-08-31 14:44:00 -0700

    afternoon practice

--- intro-to-functions.py
+++ intro-to-functions.py
@@ -152,6 +152,24 @@ print(most_popular2)
 print(most_popular3)
 
 #13/13 review
+# Write your code below:
+def trip_planner_welcome(name): 
+  print("Welcome to tripplanner v1.0 " + name)
 
+trip_planner_welcome("Will")
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(12.5)
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport="Car"):
+  print("Your trip starts off in " + origin)
+  print("And you are traveling to " + destination)
+  print("You will be traveling by " + mode_of_transport)
+  print("It will take approximately " + str(estimated_time) + " hours")
+
+destination_setup("Manila", "Vancouver", estimate, mode_of_transport="plane")
 
 
