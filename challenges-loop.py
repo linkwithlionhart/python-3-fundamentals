5114fbe63f0ceb054d67ad1523009901de814e4f
linkwithlionhart <linkwithlionhart@gmail.com>
2024-09-01 20:52:40 -0700

    more evening practice

--- challenges-loop.py
+++ challenges-loop.py
@@ -31,3 +31,26 @@
   #Uncomment the lines below when your function is done
   print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
   print(delete_starting_evens([4, 8, 10]))
+
+# Odd Indices
+  #Write your function here
+  def odd_indices(my_list):
+    new_indice = []
+    for i in range(1, len(my_list), 2):
+      new_indice.append(my_list[i])
+    return new_indice
+
+  #Uncomment the line below when your function is done
+  print(odd_indices([4, 3, 7, 10, 11, -2]))
+
+# Exponents
+  #Write your function here
+  def exponents(bases, powers):
+    answers = []
+    for base in bases:
+      for power in powers:
+        answers.append(base**power)
+    return answers
+
+  #Uncomment the line below when your function is done
+  print(exponents([2, 3, 4], [1, 2, 3]))
