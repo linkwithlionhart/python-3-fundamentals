0cd21a53a612ece89a5d4e3676937c4a01a57bcb
linkwithlionhart <linkwithlionhart@gmail.com>
2024-09-05 13:03:07 -0700

    afternoon practice

--- string-methods.py
+++ string-methods.py
@@ -113,5 +113,35 @@ my_beard_description = poem_description(
 
 print(my_beard_description)
 
-#13/13
+#13/13 review
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+# print(highlighted_poems)
 
+highlighted_poems_list = highlighted_poems.split(',')
+# print(highlighted_poems_list)
+
+highlighted_poems_stripped = []
+for poem in highlighted_poems_list:
+  highlighted_poems_stripped.append(poem.strip())
+# print(highlighted_poems_stripped)
+
+highlighted_poems_details = []
+for poem in highlighted_poems_stripped:
+  highlighted_poems_details.append(poem.split(':'))
+# print(highlighted_poems_details)
+
+titles = []
+poets = []
+dates = []
+
+for this_list in highlighted_poems_details:
+  titles.append(this_list[0])
+  poets.append(this_list[1])
+  dates.append(this_list[2])
+# print(titles, poets, dates)
+
+for this_list in highlighted_poems_details:
+  print('The poem, {TITLE}, was published by {POET} in {DATE}.'.format(TITLE=this_list[0], POET=this_list[1], DATE=this_list[2]))
+
+for i in range(len(titles)):
+  print('The poem, {TITLE}, was published by {POET} in {DATE}.'.format(TITLE=titles[i], POET=poets[i], DATE=dates[i]))
