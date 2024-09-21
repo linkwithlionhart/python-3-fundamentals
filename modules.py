347a121c181a52dc0ca33385981ad12b9a97777d
linkwithlionhart <linkwithlionhart@gmail.com>
2024-09-21 10:04:54 -0700

    morning practice

--- modules.py
+++ modules.py
@@ -31,4 +31,27 @@ plt.plot(numbers_a, numbers_b)
 plt.show()
 
 #4/6 decimal modules
+# Import Decimal below:
+from decimal import Decimal
 
+# Fix the floating point math below:
+two_decimal_points = Decimal('0.2') + Decimal('0.69')
+print(two_decimal_points)
+
+four_decimal_points = Decimal('0.53') * Decimal('0.65')
+print(four_decimal_points)
+
+#5/6 files and scope
+# Import library below:
+from library import always_three
+
+# Call your function below:
+always_three()
+
+#6/6 review
+"""
+- what modules are and how they can be useful
+- how to use a few of the most commonly used Python libraries
+- what namespaces are and how to avoid polluting your local namespace
+- how scope works for files in Python
+"""
