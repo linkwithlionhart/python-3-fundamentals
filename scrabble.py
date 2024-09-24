b0f38cea8f9fa3d6627e479089aba0494a393716
linkwithlionhart <linkwithlionhart@gmail.com>
2024-09-24 07:16:03 -0700

    more morning practice

---
+++ scrabble.py
@@ -0,0 +1,20 @@
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+# print(len(letters), len(points))
+letters_to_points = {key:value for key, value in zip(letters, points)}
+
+letters_to_points[" "] = 0
+print(letters_to_points)
+
+def score_word(word):
+  check_word = word.upper()
+  point_total = 0
+  for letter in check_word:
+    if letter in letters_to_points:
+      point_total += letters_to_points[letter]
+  print(point_total)
+  return point_total
+
+brownie_points = score_word('BROWNIE')
+
