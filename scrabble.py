9946722f5a0a871df9900d56acc8183b1c135da5
linkwithlionhart <linkwithlionhart@gmail.com>
2024-09-24 16:19:24 -0700

    afternoon practice

--- scrabble.py
+++ scrabble.py
@@ -1,3 +1,4 @@
+# Build Point Dictionary
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
@@ -7,6 +8,7 @@ letters_to_points = {key:value for key, value in zip(letters, points)}
 letters_to_points[" "] = 0
 print(letters_to_points)
 
+# Score a word
 def score_word(word):
   check_word = word.upper()
   point_total = 0
@@ -17,4 +19,23 @@ def score_word(word):
   return point_total
 
 brownie_points = score_word('BROWNIE')
+
+# Score a game
+player_to_words = {
+  "BLUE": ["EARTH", "ERASER", "ZAP"],
+  "TENNIS": ["EYES", "BELLY", "COMA"],
+  "EXIT": ["MACHINE", "HUSKY", "PERIOD"]
+}
+
+player_to_points = {}
+for player, words in player_to_words.items():
+  print(player, words)
+  player_points = 0
+  for word in words:
+    player_points += score_word(word)
+  player_to_points[player] = player_points
+  print(player_points)
+print(player_to_points)
+
+# Ideas for Further Practice!
 
