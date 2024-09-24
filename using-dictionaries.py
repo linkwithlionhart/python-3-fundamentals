9dfbb46f335d9fbe5060af7b5bc9e74b97dddb8f
linkwithlionhart <linkwithlionhart@gmail.com>
2024-09-24 06:44:58 -0700

    morning practice

--- using-dictionaries.py
+++ using-dictionaries.py
@@ -44,3 +44,47 @@ print(health_points, available_items)
 
 health_points += available_items.pop("mystic bread", 0)
 print(health_points, available_items)
+
+#6/9 get all keys with object.keys() - returns immutable object
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+users = user_ids.keys()
+lessons = num_exercises.keys()
+print(list(user_ids))
+print(list(num_exercises))
+print(users)
+print(lessons)
+
+for user in users:
+  print(user)
+
+for lesson in lessons:
+  print(lesson)
+
+#7/9 get all values with object.values()
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+for exercise in num_exercises.values():
+  total_exercises += exercise
+
+print(total_exercises)
+
+#8/9 get all items with object.items()
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for occupation, value in pct_women_in_occupation.items():
+  print(f'Women make up {value} percent of {occupation}s.')
+
+#9/9 review
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+spread["past"] = tarot.pop(13)
+spread["present"] = tarot.pop(22)
+spread["future"] = tarot.pop(10)
+print(spread)
+
+for key, value in spread.items():
+  print(f'Your {key} is the {value} card.')
