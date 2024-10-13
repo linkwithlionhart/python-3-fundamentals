0b90ddcb9692e3c04cff745f98affa4f510b9e05
linkwithlionhart <linkwithlionhart@gmail.com>
2024-10-13 07:51:35 -0700

    afternoon practice setup

---
+++ challenges-classes.py
@@ -0,0 +1,6 @@
+#1 Setting up our robot
+"""
+Create a new class called DriveBot
+Set up a basic constructor (no parameters needed)
+Initialize three instance variables within our constructor which all default to 0: motor_speed, direction, and sensor_range
+"""
