a9e7ec4eb512626da838579e0b78194a06eb700f
linkwithlionhart <linkwithlionhart@gmail.com>
2024-10-12 22:43:01 -0700

    evening practice

--- challenges_dictionary.py
+++ challenges_dictionary.py
@@ -116,3 +116,26 @@ def max_key(my_dictionary):
       largest_value = value
       largest_key = key
   return largest_key
+
+#6 Word Length Dict
+# Write your word_length_dictionary function here:
+
+# Using dictionary.update() method
+"""def word_length_dictionary(words):
+  new_dict = {}
+  for word in words:
+    new_dict.update({word:len(word)})
+  return new_dict"""
+
+# Using 
+def word_length_dictionary(words):
+  word_lengths = {}
+  for word in words:
+    word_lengths[word] = len(word)
+  return word_lengths
+
+# Uncomment these function calls to test your  function:
+print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
